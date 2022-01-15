6d758e78438e2d664883052a9df2fd89abe713b9
Pavel Sheludyakov <technician2017@mail.ru>
2022-01-15 16:55:19 +0300

    update Cipher.kt
    deleted getCharFromAlphabet

--- src/main/kotlin/core/Cipher.kt
+++ src/main/kotlin/core/Cipher.kt
@@ -1,17 +1,10 @@
 package core
 
-abstract class Cipher(
+abstract class Cipher (
     private val text: String,
     private val key: String,
     private val alphabet: String
 ) {
     abstract fun encode(): String
     abstract fun decode(): String
-    protected fun getCharFromAlphabet(index: Int): Char{
-        return alphabet[
-                if (index < 0) alphabet.length + index
-                else if (index >= alphabet.length) index - alphabet.length
-                else index
-        ]
-    }
 }
